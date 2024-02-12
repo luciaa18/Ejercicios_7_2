28c1e7a91fc332e8e82c25236f9fd47362e14845
W7 Java <W7 Java@W7JAVA-PC>
2024-02-12 11:40:51 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicios_7_2/Aminoacido.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios_7_2
+{
+    class Aminoacido
+    {
+        public struct Aminoacido
+        {
+            private int _atomosC;
+            private int _atomosN;
+            private int _atomosH;
+            private int _atomosO;
+            private int _amomosS;
+            private int _NAtomos;
+
+            private string _nombre;
+            private string _abreviaturaLarga;
+            private string _abreviaturaCorta;
+
+            private float _pesoMolecular;
+
+            private bool _esEsencial;
+            
+            //Yo lo hubiera hecho en publico
+
+            private int atomosC { get { return _atomosC; } }
+            private int atomosN { get { return _atomosN; } }
+            private int atomosH { get { return _atomosH; } }
+            private int atomosO { get { return _atomosO; } }
+            private int atomosS { get { return _amomosS; } }
+
+
+            private string nombre { get { return _nombre; } }
+            private string abreviaturaLarga { get { return _abreviaturaLarga; } }
+            private string abreviaturaCorta { get { return _abreviaturaCorta; } }
+
+
+            private float pesoMoleclar { get { return _pesoMolecular; } }
+
+
+            
+
+            private bool esEsencial { get { return _esEsencial; } }
+            //ESENCIALES:
+                //Valina (Val, V)
+                //Leucina(Leu, L)
+                //Treonina(Thr, T)
+                //Lisina(Lys, K)
+                //Triptófano(Trp, W)
+                //Histidina(His, H)
+                //Fenilalanina(Phe, F)
+                //Isoleucina(Ile, I)
+                //Arginina(Arg, R)
+                //Metionina(Met, M)
+
+
+        }
+    }
+}
---
+++ Ejercicios_7_2/DatosAminoacidos.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicios_7_2
+{
+    public class DatosAminoacidos
+    {
+        //Es una clase que solamente contiene enumeraciones, listas y diccionarios, donde se pueden encontrar todos los datos anteriores para que los puedan utilizar las demás clases.
+
+
+        //En una molécula de ADN hay una o varias proteínas codificadas(normalmente son muchísimas). Cada proteína empieza con el codón START y termina con uno de los codones STOP.
+
+        //Una proteína es una cadena de aminoácidos cuyo orden viene determinado por la secuencia de C, G, T y A de la hebra de ADN.
+
+
+        public enum TipoAminoacido
+        {
+            Alanina, Arginina, Asparagina, Acido_aspartico, Cisteina, Glutamina, Acido_glutamico, Glicina, Histidina,
+            Isoleucina, Leucina, Lisina, Metionina, Fenilalanina, Prolina, Serina, Treonina, Triptofano, Tirosina, Valina
+        }
+
+        public static Dictionary<TipoAminoacido, string> nombreAminoacidos = new Dictionary<TipoAminoacido, string>
+        {
+            {TipoAminoacido. Alanina, "Alanina" },
+            {TipoAminoacido. Arginina, "Arginina" },
+            {TipoAminoacido. Asparagina, "Asparagina" },
+            {TipoAminoacido. Acido_aspartico, "Acido_aspartico" },
+            {TipoAminoacido. Acido_glutamico, "Acido_glutamico" },
+            {TipoAminoacido. Glutamina, "Glutamina" },
+            {TipoAminoacido. Glicina, "Glicina" },
+            {TipoAminoacido. Histidina, "Histidina" },
+            {TipoAminoacido. Isoleucina, "Isoleucina" },
+            {TipoAminoacido. Leucina, "Leucina" },
+            {TipoAminoacido. Lisina, "Lisina" },
+            {TipoAminoacido. Metionina, "Metionina" },
+            {TipoAminoacido. Fenilalanina, "Fenilalanina" },
+            {TipoAminoacido. Prolina, "Prolina" },
+            {TipoAminoacido. Serina, "Serina" },
+            {TipoAminoacido. Treonina, "Treonina" },
+            {TipoAminoacido. Triptofano, "Triptofano" },
+            {TipoAminoacido. Tirosina, "Tirosina" },
+            {TipoAminoacido. Valina, "Valina" },
+        };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+        public static string ObtenerNombreAminoacido(TipoAminoacido tipo)
+        {
+            if (nombreAminoacidos.ContainsKey(tipo))
+                return nombreAminoacidos[tipo];
+            else
+                throw new ArgumentException("El tipo de aminoácido no es válido.");
+        }
+        */
+    }
+}
